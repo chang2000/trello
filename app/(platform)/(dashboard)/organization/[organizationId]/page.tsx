5281859b411835cf15ebc355040ffbf50f72c90f
Alex Wang <wangtcalex@gmail.com>
2023-11-14 18:38:31 -0800

    wip: organization

---
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -0,0 +1,15 @@
+// import { auth, OrganizationSwitcher} from '@clerk/nextjs'
+const organizationIdPage = () => {
+  // const {userId, orgId} = auth()
+  return (
+    <div>
+      Organization Page
+      {/* UserId: {userId} */}
+      {/* <OrganizationSwitcher 
+      hidePersonal */}
+      {/* /> */}
+    </div>
+  )
+}
+
+export default organizationIdPage
