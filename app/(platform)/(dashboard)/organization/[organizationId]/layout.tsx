dccb9c7f560e511fbe39be47dfa13fecee51e309
Tianchang Wang <wangtcalex@gmail.com>
2023-11-15 10:02:01 -0800

    feat: add parameter check

---
+++ app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
@@ -0,0 +1,14 @@
+import { OrgControl } from './_components/org-control'
+
+function OrganizationIdLayout({ children }: {
+  children: React.ReactNode
+}) {
+  return (
+    <>
+      <OrgControl />
+      {children}
+    </>
+  )
+}
+
+export default OrganizationIdLayout
